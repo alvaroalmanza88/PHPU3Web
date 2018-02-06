83ca1b8d56611ba523c325dba8a059704e213419
alvaroalmanza88 <31432851+alvaroalmanza88@users.noreply.github.com>
2018-02-06 17:00:36 +0100

    Ejecución correcta del dialogo modal
    Se añade la funcion del fallo del login, salta modal

--- PHPU3/autenticar.php
+++ PHPU3/autenticar.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<!--
+CODIGO REALIZADO POR ALVARO ALMANZA.
+UNIVERSIDAD INTERNACIONAL I DE CASTILLA
+-->
+<html lang="en">
+<head>
+  <title>Vehiculos on-line</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php
 $root="admin";
 $pass="admin";
@@ -6,11 +22,16 @@ $datos=$_POST["usuario"];
 if(strcmp($root, $datos["nombre"]) == 0)
     {
         echo "Usuario registrado";
+        $show_modal = false;
         include 'principal.html';
     }else
         {
-            echo "Algo ha fallado";
+            //echo "Algo ha fallado";
+            //$show_modal = true;
             include 'modal.html';
+            
         }
-
-
+?>
+    <script> $('#myModal').modal('show');</script>
+</body>
+</html>
