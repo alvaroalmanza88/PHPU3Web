a4cfc8eb2e9d6da1718bc34926d81cc72499ce05
alvaroalmanza88 <31432851+alvaroalmanza88@users.noreply.github.com>
2018-02-10 10:45:17 +0100

    implementacion de array para coches
    Se crea el array base con el que trabajar la parte de los coches

--- PHPU3/init.php
+++ PHPU3/init.php
@@ -7,4 +7,7 @@
 #
 //crea un array tipo clave-->valor
 $usuarios= array('admin'=>'admin','alvaro'=>'almanza', 'fran'=>'revuelta','ignacio'=>'alvarez','jose'=>'isidoro');
-
+$coches= array
+        (
+            array('matricula'=>'1234abc','contacto'=>'666666666','marca'=>'ford','color'=>'rojo','observaciones'=>'hola','motivo'=>'el motivo')
+        );
