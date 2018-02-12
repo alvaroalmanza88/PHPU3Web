4f82f2df54e523437e20296750718f975e258276
Alvaro Almanza <alvaro.almanza88@gmail.com>
2018-02-12 22:09:26 +0100

    muestra de los datos del usuario dado de alta

--- PHPU3/regusuario.php
+++ PHPU3/regusuario.php
@@ -4,9 +4,17 @@ CODIGO REALIZADO POR ALVARO ALMANZA.
 UNIVERSIDAD INTERNACIONAL I DE CASTILLA
 -->
 <?php
+    session_start();
     include 'init.php';
     $datos=$_POST['reguser'];
-    echo "Tamaño array antes: ".count($_SESSION["ListUser"]);
-    array_push($_SESSION["ListUser"], array('usuario'=>'$datos[nombre]', 'pass'=>'$datos[contrasenia]', 'dir'=>'$datos[direccion]', 'telefono'=>'$datos[telefono]', 'correo'=>'$datos[correo]'));
-    echo "Tamaño array antes: ". count($_SESSION["ListUser"]);
+    //echo "Tamaño array antes: ".count($_SESSION["ListUser"]);
+    array_push($_SESSION["ListUser"], array('usuario'=>$datos['nombre'], 'pass'=>$datos['contrasenia'], 'dir'=>$datos['direccion'], 'telefono'=>$datos['telefono'], 'correo'=>$datos['correo']));
+    $usr=array_pop($_SESSION["ListUser"]);
+    //echo "Tamaño array antes: ". count($_SESSION["ListUser"]);
+            /*foreach ($_SESSION["ListUser"] as $row) #recorremos el array como una serie de filas para hacer una serie de tareas
+            {
+                echo "Datos de usuario: ".$row['usuario']."<br>";      
+            }*/
+        echo "Datos de usuario dados de alta: <br>";    
+        print_r($usr);
 ?>
