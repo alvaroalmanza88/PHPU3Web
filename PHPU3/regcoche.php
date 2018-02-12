fa167444ea7c4faf0e15d79f17ca8bf6f24ef253
Alvaro Almanza <alvaro.almanza88@gmail.com>
2018-02-12 23:13:30 +0100

    cambio menor de visulizacion

--- PHPU3/regcoche.php
+++ PHPU3/regcoche.php
@@ -13,4 +13,52 @@ UNIVERSIDAD INTERNACIONAL I DE CASTILLA
     $cars=array_pop($_SESSION["ListCar"]);
     echo "Datos de coche dados de alta: <br>";    
     print_r($cars);
+ ?>
+<html>
+<head>
+    <title>Dragon Cars Registro de vehículo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
+    <link href="styles.css" rel="stylesheet" type="text/css">
+
+    <script type="text/javascript" src="bootstrap/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-default navbarHome navbarForm">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img alt="Logo Reducido Dragon cars" src="images/logoAbreviadoDragonCars.png"></a>
+                </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="indexNew.php">Inicio <span class="sr-only">(current)</span></a></li>
+                        <li><a href="regUser.php" >Alta usuario</a></li>
+                        <!--<li><a href="#" >L</a></li>-->
+                        <li class="active"><a>Alta vehículo</a></li>
+                        <li><a href="delcoche.php">Baja vehículo</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+    </header>
+    <section class="formularioRegistro"> 
+    </section>
+
+</body>
+</html>
