99766a91e2acf202fe6750aaf25a15e1a8358b5c
Alvaro Almanza <alvaro.almanza88@gmail.com>
2018-02-12 22:43:28 +0100

    implementacion para mostrar el ultimo elemento del array para comprobar el alta

--- PHPU3/regcoche.php
+++ PHPU3/regcoche.php
@@ -4,9 +4,13 @@ CODIGO REALIZADO POR ALVARO ALMANZA.
 UNIVERSIDAD INTERNACIONAL I DE CASTILLA
 -->
 <?php
-    include 'init.php';
+    session_start();
+    //include 'init.php';
     $datos=$_POST['car'];
-    echo "Tamaño array antes: ".count($_SESSION["ListUser"]);
-    array_push($_SESSION["ListCar"], array('matricula'=>'$datos[matricula]', 'contacto'=>'$datos[telefono]', 'marca'=>'$datos[marca]', 'color'=>'$datos[color]', 'observaciones'=>'$datos[observaciones]','motivo'=>'$datos[motivo]'));
-    echo "Tamaño array antes: ". count($_SESSION["ListUser"]);
+    //echo "Tamaño array antes: ".count($_SESSION["ListUser"]);
+    array_push($_SESSION["ListCar"], array('matricula'=>$datos['matricula'], 'contacto'=>$datos['telefono'], 'marca'=>$datos['marca'], 'color'=>$datos['color'], 'observaciones'=>$datos['observaciones'],'motivo'=>$datos['motivo']));
+    //echo "Tamaño array antes: ". count($_SESSION["ListUser"]);
+    $cars=array_pop($_SESSION["ListCar"]);
+    echo "Datos de coche dados de alta: <br>";    
+    print_r($cars);
 
