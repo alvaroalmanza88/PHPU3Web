04757e49b7f9c3da0027793e835f54f44ecf2487
Alvaro Almanza <alvaro.almanza88@gmail.com>
2018-02-12 18:18:30 +0100

    cambio menor

--- PHPU3/init.php
+++ PHPU3/init.php
@@ -6,7 +6,9 @@
 #UNIVERSIDAD INTERNACIONAL I DE CASTILLA
 #
 //crea un array tipo clave-->valor
-$usuarios= array('admin'=>'admin','alvaro'=>'almanza', 'fran'=>'revuelta','ignacio'=>'alvarez','jose'=>'isidoro');
+$usuarios= array(
+    array('usuario'=>'admin', 'pass'=>'admin', 'dir'=>'calle madrid', 'telefono'=>'111111111', 'correo'=>'hola@hola'),
+    array('usuario'=>'alvaro', 'pass'=>'almanza', 'dir'=>'calle madrid', 'telefono'=>'111111111', 'correo'=>'hola@hola'));
 $coches= array
         (
             array('matricula'=>'1234abc','contacto'=>'666666666','marca'=>'ford','color'=>'rojo','observaciones'=>'hola','motivo'=>'el motivo')
