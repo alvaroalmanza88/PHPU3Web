bef3778b29067c2dc6540a39d1e73c082fd8ee36
Alvaro Almanza <alvaro.almanza88@gmail.com>
2018-02-12 23:26:31 +0100

    implementacion eliminacion de usuario

---
+++ PHPU3/deluser.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+    <title>Dragon Cars Eliminar coche</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
+    <link href="styles.css" rel="stylesheet" type="text/css">
+
+    <script type="text/javascript" src="bootstrap/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-default navbarHome navbarForm">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img alt="Logo Reducido Dragon cars" src="images/logoAbreviadoDragonCars.png"></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="indexNew.php">Inicio <span class="sr-only">(current)</span></a></li>
+                    <li><a>Alta usuario</a></li>
+                    <!--<li><a href="#">Loguéate</a></li>-->
+                    <li><a href="regVehicle.php">Alta vehículo</a></li>
+                    <li><a href="delcoche.php">Baja vehículo</a></li>
+                    <li class="active"><a href="deluser.php">Baja usuario</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</header>
+<section class="formularioRegistro">
+    <h2>Eliminar Coche de la base de datos</h2>
+            <form role="form" action="rmuser.php" method="POST">
+                    <div class="form-group col-md-6">
+                    <input type="text" class="form-control" name="deluser" placeholder ="telefono del usuario" required>
+                    </div>
+                    <input type="submit" value="Eliminar" class="btn btn-default">
+            </form>
+     
+        <div class="form-group col-md-2">
+        </div>
+    </form>
+</section>
+
+</body>
+</html>
+
